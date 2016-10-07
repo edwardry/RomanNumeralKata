56cdd6f42e85e8d8e265a7e59dd1b780c5e90bf1
edwardry <ryan.edwards03@gmail.com>
2016-10-06 23:26:39 -0400

    Forgot to update the method names of my test cases after changes the name of the convert arabic to roman numeral method to valueOf

--- test/RomanNumeralTest.java
+++ test/RomanNumeralTest.java
@@ -4,23 +4,23 @@ import static junit.framework.TestCase.assertEquals;
 
 public class RomanNumeralTest {
     @Test
-    public void whenConvertFromArabicToRomanNumeralIsPassedOneItReturnsI() {
+    public void whenValueOfRomanNumeralIsPassedOneItReturnsI() {
        assertEquals("I", RomanNumeral.valueOf(1));
     }
 
     @Test
-    public void whenConvertFromArabicToRomanNumeralIsPassedThreeItReturnsIII() {
+    public void whenValueOfRomanNumeralIsPassedThreeItReturnsIII() {
         assertEquals("III", RomanNumeral.valueOf(3));
     }
 
     @Test
-    public void whenConvertFromArabicToRomanNumeralIsPassedAValueUnderTenThatRequiresNumeralSubtraction() {
+    public void whenValueOfRomanNumeralIsPassedAValueUnderTenThatRequiresNumeralSubtraction() {
         assertEquals("IV", RomanNumeral.valueOf(4));
         assertEquals("IX", RomanNumeral.valueOf(9));
     }
 
     @Test
-    public void whenConvertFromArabicToRomanNumeralIsPassedValuesOfOneHundredOrLess() {
+    public void whenValueOfRomanNumeralIsPassedValuesOfOneHundredOrLess() {
         assertEquals("L", RomanNumeral.valueOf(50));
         assertEquals("C", RomanNumeral.valueOf(100));
         assertEquals("XLVI", RomanNumeral.valueOf(46));
@@ -29,7 +29,7 @@ public class RomanNumeralTest {
     }
 
     @Test
-    public void whenConvertFromArabicToRomanNumeralIsPassedValuesOverOneHundred() {
+    public void whenValueOfRomanNumeralIsPassedValuesOverOneHundred() {
         assertEquals("D", RomanNumeral.valueOf(500));
         assertEquals("M", RomanNumeral.valueOf(1000));
         assertEquals("CD", RomanNumeral.valueOf(400));
@@ -41,7 +41,7 @@ public class RomanNumeralTest {
     }
 
     @Test
-    public void whenConvertFromArabicToRomanNumeralIsPassedValuesThatAreInvalid() {
+    public void whenValueOfRomanNumeralIsPassedValuesThatAreInvalid() {
         assertEquals("I'm sorry. I don't know how to convert -1046. :(", RomanNumeral.valueOf(-1046));
         assertEquals("I'm sorry. I don't know how to convert 0. :(", RomanNumeral.valueOf(0));
         assertEquals("I'm sorry. I don't know how to convert 4000. :(", RomanNumeral.valueOf(4000));
